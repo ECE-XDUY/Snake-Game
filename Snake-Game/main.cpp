561d43d84075bd39cff0172ed86589ce07a2fe54
Phan Trịnh Xuân Duy <17926@student.vgu.edu.vn>
2022-01-12 23:09:31 +0700

    Main.cpp
    - updated input and playfield
    - playfield is now called draw
    - input and draw definition included in main.cpp

--- Snake-Game/main.cpp
+++ Snake-Game/main.cpp
@@ -1,49 +1,81 @@
 #include <iostream>
 #include <cstdio>
 #include <conio.h>
-#include "playfield.h"
 using namespace std;
 
 bool gameOver;
+const int width = 40.0;
+const int height = 12.0;
 int x, y, fruitX, fruitY, score;
 enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
 void input();
+void draw();
 
 //============ main program =============
 int main()
 {
 
-    printfield();
+    draw();
+    input();
 
     system("pause");
     return 0;
 }
 
 //========= Function Definition =========
-//void input()
-//{
-// char input;
-// std::cin >> input;
-// if (input == "w"){
-//   if (dir == UP){}
-//   else dir == UP;
-// }
-//
-// if (input == "a"){
-//   if (dir == LEFT){}
-//   else dir == LEFT;
-// }
-//
-// if (input == "s"){
-//   if (dir == DOWN){}
-//   else dir == DOWN;
-// }
-//
-// if (input == "d"){
-//   if (dir == RIGHT){}
-//   else dir == RIGHT;
-// }
-//}
+void input()
+{
+  if (_kbhit())
+  {
+      switch (_getch())
+      {
+      case 'a':
+         dir = LEFT;
+         break;
 
+      case 'd':
+         dir = RIGHT;
+         break;
+
+      case 'w':
+         dir = UP;
+         break;
+
+      case 's':
+         dir = DOWN;
+         break;
+
+      case 'x':
+         gameOver = true;
+         break;
+      }
+  }
+}
+
+void draw(){
+     system("cls");
+     for (int i = 0; i < height; i++){
+
+     if (i == 0 || i == height - 1)
+    {
+        for (int j = 0; j < width; j++)
+           cout << "=";
+        cout << "\n";
+    }
+     else
+    {
+        cout << "#";
+        for (int j = 0; j < width - 2; j++)
+        {
+           cout << " ";
+           if (i == y && j == x)
+              cout << "O";
+           else if (i == fruitY && j == fruitX)
+              cout << "F";
+        }
+        cout << "#\n";
+    }
+  }
+}
